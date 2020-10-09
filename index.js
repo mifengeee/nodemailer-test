3a3f0b95709d85a0fea12c44d012946481f1e9b0
yangguang <719043997@qq.com>
2020-10-09 17:04:29 +0800

    完善注释

--- index.js
+++ index.js
@@ -6,32 +6,32 @@ const nodemailer = require("nodemailer");
 app.get("/sendMail",(req,res) => {
     // 第一步
     let transporter = nodemailer.createTransport({
-        service:"qq",  //  邮箱
+        service:"qq",   //  邮箱类型 如果用163 就填163
         secure:true,    //  安全的发送模式
         auth:{
-            user:"719043997@qq.com", //  全局变量
-            pass:"***********"//  授权码
+            user:"111111111@qq.com", //  全局变量
+            pass:"***********"       //  IMAP/SMTP授权码
         }
     })
     // 第二步
     let mailOptions = {
-        from:"719043997@qq.com",
-        to:"274005538@qq.com",
-        subject:"测试title",
-        text:"测试message"
+        from:"1111111111@qq.com",   //发件人邮箱
+        to:"2222222222@qq.com",     //收件人邮箱
+        subject:"测试title",        //邮件标题
+        text:"测试message"          //邮件正文
     }
     // 第三步
     transporter.sendMail(mailOptions,(err,data) => {
         if(err){
             console.log(err);
-            res.json({status:400,msg:"send fail....."})
+            res.json({status:400,msg:"发送失败！"})
         }else{
             console.log(data);
-            res.json({status:200,msg:"邮件发送成功....."})
+            res.json({status:200,msg:"邮件发送成功啦！"})
         }
     })
 
 })
 app.listen(3000,() => {
-    console.log("服务开启在3000端口...");
+    console.log("服务开启在3000端口");
 })
